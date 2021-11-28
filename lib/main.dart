5f3aef795f498d2c4c6635bc5b6b64eb6f6a98db
Sam Thorn <spthornnz@gmail.com>
2021-11-28 19:02:00 +1300

    Adds card 3 toru to pageView children

--- lib/main.dart
+++ lib/main.dart
@@ -95,6 +95,16 @@ class TeReoCardWidget extends StatelessWidget {
             ),
           ),
         ),
+        Center(
+          child: Container(
+            width: 360,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/card3.png'),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
