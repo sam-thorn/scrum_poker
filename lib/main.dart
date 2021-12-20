dcebca65f2d45ed1eea5b232f235ae73069867dc
Sam Thorn <spthornnz@gmail.com>
2021-12-20 21:39:55 +1300

    Adds card 100 kotahi rau

--- lib/main.dart
+++ lib/main.dart
@@ -155,6 +155,16 @@ class TeReoCardWidget extends StatelessWidget {
             ),
           ),
         ),
+        Center(
+          child: Container(
+            width: 360,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/card100.png'),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
